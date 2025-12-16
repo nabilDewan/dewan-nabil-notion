66f7e50af48e0821420806bf916cdd9b17fc6840
nabilDewan <47483859+nabilDewan@users.noreply.github.com>
2025-12-16 21:10:41 +0000

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,17 +9,17 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Dewan Hafiz Nabil',
+  domain: 'dewanhafiznabil.com',
+  author: 'Dewan Hafiz Nabil',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Industrial Engineer| PhD Researcher| @WMG, University of Warwick| Specializing in Clean Energy Supply Chains',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'DewanNabil14',
+  github: '',
+  linkedin: 'dh-nabil',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
